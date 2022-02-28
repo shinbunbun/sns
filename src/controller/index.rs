e18a7a69f4a24dd2e4ed323cbfbab2d834e45bcd
shinbunbun <takamuneyuto@gmail.com>
2022-02-28 07:32:43 +0000

    indexをaskamaに変更

--- src/controller/index.rs
+++ src/controller/index.rs
@@ -1,5 +1,17 @@
 use actix_web::{HttpResponse, Responder};
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+
+struct IndexTemplate<'a> {
+    name: &'a str,
+}
 
 pub async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    let html = IndexTemplate { name: "rust" };
+    let response_body = html.render().expect("Create response body error");
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body)
 }
