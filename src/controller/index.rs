39a150c5f906f4d398ccde125e4030093ce2bdd1
shinbunbun <takamuneyuto@gmail.com>
2022-02-28 08:06:15 +0000

    templateに挿入するinputをviews以下に移動

--- src/controller/index.rs
+++ src/controller/index.rs
@@ -1,15 +1,11 @@
+use crate::views;
 use actix_web::{HttpResponse, Responder};
 use askama::Template;
 
-#[derive(Template)]
-#[template(path = "index.html")]
-
-struct IndexTemplate<'a> {
-    name: &'a str,
-}
-
 pub async fn index() -> impl Responder {
-    let html = IndexTemplate { name: "rust" };
+    let html = views::index::IndexTemplate {
+        name: String::from("rust"),
+    };
     let response_body = html.render().expect("Create response body error");
     HttpResponse::Ok()
         .content_type("text/html")
---
+++ src/views/index.rs
@@ -0,0 +1,8 @@
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+
+pub struct IndexTemplate {
+    pub name: String,
+}
