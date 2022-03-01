0c30bb82015f33bfe40fe6b7df0aa3468d88fb0f
shinbunbun <takamuneyuto@gmail.com>
2022-03-01 03:22:34 +0000

    signupページを実装

--- src/router.rs
+++ src/router.rs
@@ -3,4 +3,5 @@ use actix_web::web;
 
 pub fn router(cfg: &mut web::ServiceConfig) {
     cfg.route("/", web::get().to(controller::index::index));
+    cfg.route("/signup", web::get().to(controller::signup::signup));
 }
