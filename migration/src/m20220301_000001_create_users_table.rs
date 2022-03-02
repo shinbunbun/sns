f8ac3eb27b257425c9e91e727844215c4a071701
shinbunbun <takamuneyuto@gmail.com>
2022-03-02 07:47:49 +0000

    created_at, updated_atを修正

--- migration/src/m20220301_000001_create_users_table.rs
+++ migration/src/m20220301_000001_create_users_table.rs
@@ -14,37 +14,34 @@ impl MigrationName for Migration {
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .create_table(
-                Schema::new(DbBackend::MySql).create_table_from_entity(Entity),
-                /* Table::create()
-                .table(Entity)
-                .if_not_exists()
-                .col(
-                    ColumnDef::new(Column::UserId)
-                        .string_len(26)
-                        .not_null()
-                        .primary_key(),
-                )
-                .col(ColumnDef::new(Column::UserName).string_len(50).not_null())
-                .col(ColumnDef::new(Column::EMail).string_len(254).not_null())
-                .col(
-                    ColumnDef::new(Column::PasswordHash)
-                        .string_len(128)
-                        .not_null(),
-                )
-                .col(
-                    ColumnDef::new(Column::CreatedAt)
-                        .date_time()
-                        .timestamp()
-                        .not_null(),
-                )
-                .col(
-                    ColumnDef::new(Column::UpdatedAt)
-                        .date_time()
-                        // .timestamp()
-                        .not_null(),
-                )
-                .to_owned(), */
+            .create_table(Schema::new(DbBackend::MySql).create_table_from_entity(Entity))
+            .await?;
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Entity)
+                    .modify_column(
+                        ColumnDef::new(Column::CreatedAt)
+                            .date_time()
+                            .not_null()
+                            .extra("DEFAULT CURRENT_TIMESTAMP".to_string()),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Entity)
+                    .modify_column(
+                        ColumnDef::new(Column::UpdatedAt)
+                            .date_time()
+                            .not_null()
+                            .extra(
+                                "DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP".to_string(),
+                            ),
+                    )
+                    .to_owned(),
             )
             .await
     }
--- migration/src/m20220301_000003_create_messages_table.rs
+++ migration/src/m20220301_000003_create_messages_table.rs
@@ -14,8 +14,19 @@ impl MigrationName for Migration {
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .create_table(
-                Schema::new(DbBackend::MySql).create_table_from_entity(Entity),
+            .create_table(Schema::new(DbBackend::MySql).create_table_from_entity(Entity))
+            .await?;
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Entity)
+                    .modify_column(
+                        ColumnDef::new(Column::CreatedAt)
+                            .date_time()
+                            .not_null()
+                            .extra("DEFAULT CURRENT_TIMESTAMP".to_string()),
+                    )
+                    .to_owned(),
             )
             .await
     }
