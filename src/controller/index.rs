b6ef7ea25baa838a4a2fabb899d2bc067c49f9fc
shinbunbun <takamuneyuto@gmail.com>
2022-03-04 01:26:38 +0000

    contextを使用するよう修正

--- src/controller/index.rs
+++ src/controller/index.rs
@@ -1,23 +1,21 @@
 use sha3::{Digest, Sha3_256};
 
+use crate::app_context::AppContext;
 use crate::usecase;
 use crate::{views, views::TemplateToResponse};
 use actix_web::{web, HttpResponse, Responder};
-use sea_orm::Database;
 use serde::Deserialize;
 
 pub async fn index() -> impl Responder {
     views::index::IndexTemplate {}.to_response()
 }
 
-pub async fn index_post(req: web::Form<Req>) -> impl Responder {
-    let db = Database::connect("mysql://user:password@db/local_db")
-        .await
-        .unwrap();
+pub async fn index_post(req: web::Form<Req>, context: web::Data<AppContext>) -> impl Responder {
+    let db = &context.db;
     let mut hasher = Sha3_256::new();
     hasher.update(&req.password);
     let password_hash = hex::encode(hasher.finalize());
-    let select_result = usecase::user::select_by_e_mail(&db, &req.email).await;
+    let select_result = usecase::user::select_by_e_mail(db, &req.email).await;
     let select_result = match select_result {
         Ok(res) => res,
         Err(_) => return HttpResponse::InternalServerError().body("database select error"),
