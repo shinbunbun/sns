7a7cbd58ae8f1f4ef80e8ce0824c08f964cb93ea
shinbunbun <takamuneyuto@gmail.com>
2022-03-04 10:55:57 +0000

    投稿の表示処理を実装

--- src/controller/timeline.rs
+++ src/controller/timeline.rs
@@ -74,26 +74,4 @@ pub async fn timeline(context: web::Data<AppContext>, session: Session) -> impl 
         posts,
     }
     .to_response()
-    /* views::timeline::TimelineTemplate {
-        user_name: String::from("rust"),
-        posts: vec![
-            views::timeline::Post {
-                message_id: String::from("1"),
-                user_name: String::from("test"),
-                message_text: String::from("content"),
-                created_at: Local::now(),
-                likes: 3,
-                is_like: true,
-            },
-            views::timeline::Post {
-                message_id: String::from("2"),
-                user_name: String::from("test2"),
-                message_text: String::from("content2"),
-                created_at: Local::now(),
-                likes: 0,
-                is_like: false,
-            },
-        ],
-    }
-    .to_response() */
 }
