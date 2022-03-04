4e729742d05275603227dd5eb572e8cb41120b61
shinbunbun <takamuneyuto@gmail.com>
2022-03-04 03:44:54 +0000

    signup時にsessionを追加する処理を実装

--- src/controller/signup.rs
+++ src/controller/signup.rs
@@ -1,3 +1,4 @@
+use actix_session::Session;
 use sha3::{Digest, Sha3_256};
 
 use crate::app_context::AppContext;
@@ -11,7 +12,11 @@ pub async fn signup() -> impl Responder {
     views::signup::SignUpTemplate {}.to_response()
 }
 
-pub async fn signup_post(req: web::Form<Req>, context: web::Data<AppContext>) -> impl Responder {
+pub async fn signup_post(
+    req: web::Form<Req>,
+    context: web::Data<AppContext>,
+    session: Session,
+) -> impl Responder {
     if req.validate().is_err() {
         return HttpResponse::BadRequest().body("validate error");
     }
@@ -20,12 +25,17 @@ pub async fn signup_post(req: web::Form<Req>, context: web::Data<AppContext>) ->
     hasher.update(&req.password);
     let password_hash = hex::encode(hasher.finalize());
     let insert_result = usecase::user::insert(db, &req.email, &password_hash, &req.name).await;
-    match insert_result {
-        Ok(_) => HttpResponse::SeeOther()
-            .insert_header(("Location", "timeline"))
-            .finish(),
-        Err(_) => HttpResponse::InternalServerError().body("database insert error"),
-    }
+    let insert_result = match insert_result {
+        Ok(res) => res,
+        Err(_) => return HttpResponse::InternalServerError().body("database insert error"),
+    };
+    match session.insert("user", insert_result.last_insert_id) {
+        Ok(_) => (),
+        Err(_) => return HttpResponse::InternalServerError().body("session insert failed"),
+    };
+    HttpResponse::SeeOther()
+        .insert_header(("Location", "timeline"))
+        .finish()
 }
 
 #[derive(Debug, Validate, Deserialize)]
