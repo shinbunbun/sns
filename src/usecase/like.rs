6f9bb1009e4715f08f3f9e0dfe4ad5d84cca4751
shinbunbun <takamuneyuto@gmail.com>
2022-03-07 01:46:33 +0000

    likeのinsertを実装

---
+++ src/usecase/like.rs
@@ -0,0 +1,14 @@
+use sea_orm::{DbConn, DbErr, EntityTrait, InsertResult, Set};
+use ulid::Ulid;
+
+pub async fn insert(
+    db: &DbConn,
+    user_id: &str,
+    message_id: &str,
+) -> Result<InsertResult<entity::like::ActiveModel>, DbErr> {
+    let model = entity::like::ActiveModel {
+        user_id: Set(user_id.to_owned()),
+        message_id: Set(message_id.to_owned()),
+    };
+    entity::like::Entity::insert(model).exec(db).await
+}
