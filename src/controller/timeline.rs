6ef8970d37e3f33627bbed8c20ab33d85bd3d7fb
shinbunbun <takamuneyuto@gmail.com>
2022-03-07 01:50:10 +0000

    formatを修正

--- src/controller/timeline.rs
+++ src/controller/timeline.rs
@@ -7,21 +7,22 @@ pub async fn timeline() -> impl Responder {
         user_name: String::from("rust"),
         posts: vec![
             views::timeline::Post {
-            message_id: String::from("1"),
-            user_name: String::from("test"),
-            message_text: String::from("content"),
-            created_at: Local::now(),
-            likes: 3,
-            is_like: true,
-        },
-        views::timeline::Post{
-            message_id: String::from("2"),
-            user_name: String::from("test2"),
-            message_text: String::from("content2"),
-            created_at: Local::now(),
-            likes: 0,
-            is_like: false,
-        }],
+                message_id: String::from("1"),
+                user_name: String::from("test"),
+                message_text: String::from("content"),
+                created_at: Local::now(),
+                likes: 3,
+                is_like: true,
+            },
+            views::timeline::Post {
+                message_id: String::from("2"),
+                user_name: String::from("test2"),
+                message_text: String::from("content2"),
+                created_at: Local::now(),
+                likes: 0,
+                is_like: false,
+            },
+        ],
     }
     .to_response()
 }
